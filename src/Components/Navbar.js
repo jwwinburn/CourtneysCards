d908a359f15b9ac669e2e3215c514121106fd421
Jerry Winburn <jerrywwinburn@gmail.com>
2023-06-16 11:44:12 -0500

    logo centering

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
         <div>
       <div className="navbar bg-primary text-primary-content flex justify-between">
-        <div className="flex justify-around">
+        <div className="w-4/12 flex justify-around">
           <a className="font-sans btn btn-ghost normal-case text-xl">
             Special Occasions
           </a>
@@ -14,11 +14,11 @@ function Navbar() {
           <a className="btn btn-ghost normal-case text-xl">Misc</a>
         </div>
         
-        <div className="max-w-[1rem] flex justify-center">
+        <div className="w-4/12 flex justify-center">
           <img className="max-h-12" src="images/JCSG_Logo.png" />
         </div>
 
-        <div className="flex justify-around">
+        <div className="w-4/12 flex justify-end pr-5">
           <input
             type="text"
             placeholder="Search here..."
