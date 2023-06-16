0861802a3f95ee73b22e5ea53e5dd89f017c4212
Jerry Winburn <jerrywwinburn@gmail.com>
2023-06-16 11:25:28 -0500

    Nav bar update

---
+++ src/Components/Test.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Test() {
+  return (
+    <div className="flex justify-between">
+      <div className="flex justify-around w-4/12">
+        <div>test</div>
+        <div>test</div>
+        <div>test</div> 
+      </div>
+      <div className="flex justify-center w-4/12">
+        <div>Logo</div>
+      </div>
+      <div className="flex justify-around w-4/12">
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+      </div>
+    </div>
+  );
+}
+
+export default Test;
