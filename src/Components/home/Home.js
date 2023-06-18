3ea526d0df93a3fa771c9771e4eb330a96214aa0
Alex Bishop <alexb98@gmail.com>
2023-06-18 10:44:53 -0500

    Mailing List

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Navbar from "../Navbar";
 import Hero from "../Hero";
 import Carousel from "../Carousel";
+import { MailingList } from "../MailingList";
 export const Home = () => {
   return (
     <div>
       <Hero />
       <Carousel />
+      <MailingList />
     </div>
   );
 };
