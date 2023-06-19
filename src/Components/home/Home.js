9a2b4dbb293f953ea0afec4a084a94bf9c5b4cc1
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-06-19 11:41:15 -0400

    footer work

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../Hero";
 import Carousel from "../Carousel";
 import { MailingList } from "../MailingList";
-import Footer from "../Footer/Footer";
 import CelebrateYourGrad from "../CelebrateYourGrad/CelebrateYourGrad";
 
 export const Home = () => {
